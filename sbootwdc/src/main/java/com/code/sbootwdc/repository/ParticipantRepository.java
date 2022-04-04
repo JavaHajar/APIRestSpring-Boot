56940da13a4a627df60a9352df162ef71916aa70
hajar-co <boudali.hajar@gmail.com>
2022-04-04 12:49:03 +0000

    adding file participant repository

--- sbootwdc/src/main/java/com/code/sbootwdc/repository/ParticipantRepository.java
+++ sbootwdc/src/main/java/com/code/sbootwdc/repository/ParticipantRepository.java
@@ -1,4 +1,9 @@
 package com.code.sbootwdc.repository;
 
-public interface ParticipantRepository {
+import com.code.sbootwdc.model.Participant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParticipantRepository extends JpaRepository<Participant, Integer> {
 }
