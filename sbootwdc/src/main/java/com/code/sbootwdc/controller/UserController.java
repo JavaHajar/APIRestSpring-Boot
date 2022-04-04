7c9b40fd403464dc67eb1d7465fb04b2e9662a26
hajar-co <boudali.hajar@gmail.com>
2022-04-04 12:47:21 +0000

    crud for responsables

--- sbootwdc/src/main/java/com/code/sbootwdc/controller/UserController.java
+++ sbootwdc/src/main/java/com/code/sbootwdc/controller/UserController.java
@@ -1,18 +1,21 @@
 package com.code.sbootwdc.controller;
-import com.code.sbootwdc.model.Admin;
-import com.code.sbootwdc.model.Role;
-import com.code.sbootwdc.model.User;
+import com.code.sbootwdc.model.*;
 import com.code.sbootwdc.service.admin.AdminService;
+import com.code.sbootwdc.service.responsable.ResponsableService;
 import com.code.sbootwdc.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
     @Autowired
     AdminService adminService;
+
+    @Autowired
+    ResponsableService responsableService;
 
     //     endpoints for admin  =============
 
@@ -22,4 +25,60 @@ public class UserController {
     }
 
     //     endpoints for responsable  =============
+
+    @GetMapping("/responsables")
+    public List<Responsable> getresponsables() {
+        return (List<Responsable>) responsableService.findAll();
+    }
+
+    @GetMapping ("/responsable/{id}")
+    public Optional<Responsable> getOnerespons(@PathVariable("id") Integer id){
+        return responsableService.findById(id);
+    }
+
+    @PostMapping("/createresponsable")
+    public Responsable saveresponsable(Responsable responsable){
+        return responsableService.save(responsable);
+    }
+
+    @PutMapping("/updateresponsable")
+    public String updatresponsable(Responsable responsable){
+        responsableService.update(responsable);
+        return "responsable updated succefully";
+    }
+
+    @DeleteMapping("/deletresponsable")
+    public String deletresponsable(Responsable responsable){
+        responsableService.delete(responsable);
+        return "responsable deleted succefully";
+    }
+
+    //     endpoints for participant  =============
+    @GetMapping("/participants")
+    public List<Responsable> getresponsables() {
+        return (List<Responsable>) responsableService.findAll();
+    }
+
+    @GetMapping ("/participant/{id}")
+    public Optional<Responsable> getOnerespons(@PathVariable("id") Integer id){
+        return responsableService.findById(id);
+    }
+
+    @PostMapping("/creatparticipant")
+    public Responsable saveresponsable(Responsable responsable){
+        return responsableService.save(responsable);
+    }
+
+    @PutMapping("/updatparticipant")
+    public String updatresponsable(Responsable responsable){
+        responsableService.update(responsable);
+        return "responsable updated succefully";
+    }
+
+    @DeleteMapping("/deletparticipants")
+    public String deletresponsable(Responsable responsable){
+        responsableService.delete(responsable);
+        return "responsable deleted succefully";
+    }
+
 }
---
+++ sbootwdc/src/main/java/com/code/sbootwdc/repository/ParticipantRepository.java
@@ -0,0 +1,4 @@
+package com.code.sbootwdc.repository;
+
+public interface ParticipantRepository {
+}
