a24fd8e375081634312383905ac330a13af9ec4b
hajar-co <boudali.hajar@gmail.com>
2022-05-09 15:31:24 +0100

    adding all cruds

--- sbootwdc/src/main/java/com/code/sbootwdc/repository/TyperesponsabRepos.java
+++
@@ -1,7 +0,0 @@
-package com.code.sbootwdc.repository;
-
-import com.code.sbootwdc.model.Typeresponsable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TyperesponsabRepos extends JpaRepository <Typeresponsable,Integer> {
-}
